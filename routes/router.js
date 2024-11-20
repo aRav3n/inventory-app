fd65a9dee2c7489fa04cf481a697a6446b749a1e
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-11-20 13:49:11 -0500

    build: update router.js

--- routes/router.js
+++ routes/router.js
@@ -3,9 +3,14 @@ const controller = require("../controllers/controller");
 const router = Router();
 
 router.get("/", controller.indexActionGet);
-router.get("/new", controller.newActionGet);
-router.post("/new", controller.newActionPost);
-router.get("/search", controller.searchActionGet);
+router.post("/newCategory", controller.newCategoryPost);
+router.post("/newItem", controller.newItemPost);
+router.post("/updateCategory", controller.updateCategoryPost);
+router.post("/updateItem", controller.updateItemPost);
+router.post("/updateList", controller.updateLis);
+router.post("/deleteCategory", controller.deleteCategoryPost);
+router.post("/deleteItem", controller.deleteItemPost);
+
 router.use("*", controller.errorGet);
 
 module.exports = router;
